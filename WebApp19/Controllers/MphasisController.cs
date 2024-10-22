09aea031dde0a00340c0c28f37080486c5f6ee56
sureshreddyrayapati <resuresh0@gmail.com>
2024-10-22 21:56:10 +0530

    last

--- WebApp19/Controllers/MphasisController.cs
+++ WebApp19/Controllers/MphasisController.cs
@@ -33,6 +33,58 @@ namespace WebApp19.Controllers
             }
             return View(newMphasis);
         }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null|| id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _context.mphases.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Mphasis obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.mphases.Update(obj);
+              _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if(id == null|| id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _context.mphases.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Mphasis mphasis)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.mphases.Remove(mphasis);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
  
